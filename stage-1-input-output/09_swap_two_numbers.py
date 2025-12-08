be43fac68ac3d8594534cc3f766c043e0c0fee59
Mohammad Arqam <arkamarkam554@gmail.com>
2025-12-08 20:05:46 +0530

    Add Stage 1 basic math and input-output programs
    Uploaded beginner-level Python programs covering basic input/output,
    mathematical calculations, and logic building as part of Stage 1 practice.

---
+++ stage-1-input-output/09_swap_two_numbers.py
@@ -0,0 +1,10 @@
+# Problem 9 — Swap two numbers
+a = 10
+b = 5
+
+temp = a
+a = b
+b = temp
+
+print("a =",a)
+print("b =",b)
---
+++ stage-1-input-output/10_positive_negative_zero.py
@@ -0,0 +1,9 @@
+# Problem 10 — Positive / negative check
+
+n = int(input("Enter number:"))
+
+if n > 0:
+    print("Given number is positive")
+elif n < 0:
+    print("Given number is negative")
+else : print("Given number is 0")
